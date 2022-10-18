1330bf8c561cf29c6cae882d2832b88f8983618f
leebuhrmann <lee.buhrmann@gmail.com>
2022-10-17 21:01:49 -0500

    Completed basics.
    Collects data from list and prints data.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,73 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.Calendar;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        int[] p1 = new int[26];
+        int[] p2 = new int[26];
+        int[] p3 = new int[26];
+        int[] p4 = new int[26];
+        int[] p5 = new int[26];
+
+        
+        /*
+         * learning
+         */
+        // p1[0]++;
+        // p2[0]++;
+        // p2[0]++;
+        // System.out.print(p1[0] + " | " + p2[0]);
+        // int ch = 'Z';
+        // System.out.println(" | " + (ch - 65));
+        // System.out.printf("|%3d|\n", 1);
+        // System.out.printf("%c", 0 + 65);
+        // System.exit(0);
+
+
+        int num = -1;
+        String word = null;
+        
+        try
+        {
+
+            Scanner in = new Scanner(new File("src/Wordle Answers.txt"));
+            in.nextLine(); //skips first line
+
+            while(in.hasNextLine() && num < 2314)
+            {
+                String record = in.nextLine();
+                Scanner lineParser = new Scanner(record);
+
+                lineParser.next(); //skips date
+                num = lineParser.nextInt();
+                word = lineParser.next();
+                word = word.toUpperCase();
+
+                char[] wordParser = word.toCharArray();
+                
+                p1[wordParser[0] - 65]++;
+                p2[wordParser[1] - 65]++;
+                p3[wordParser[2] - 65]++;
+                p4[wordParser[3] - 65]++;
+                p5[wordParser[4] - 65]++;
+            }
+        }
+        catch(FileNotFoundException fnfe)
+        {
+            System.out.println("File not found!");
+        }
+
+        System.out.printf("\n***|  1  |  2  |  3  |  4  |  5  |***\n");
+        for(int i = 0; i < 26; i++)
+        {
+            System.out.printf(" %c | %3d | %3d | %3d | %3d | %3d |\n"
+                                , (i + 65), p1[i], p2[i], p3[i], p4[i], p5[i]);
+        }
+
+        System.out.printf("\nThe final word scanned was %s and was number %d\n\n", word, num);
     }
 }
